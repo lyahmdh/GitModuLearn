19ea8fca313a34c282814cae39dca08669029e3b
Alya Hamidah <lyahamidah14@gmail.com>
2025-11-17 09:28:15 +0700

    ubah

--- ModuLearn/app/Models/Course.php
+++ ModuLearn/app/Models/Course.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Course extends Model
 {
@@ -11,11 +12,36 @@ class Course extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'description',
+        'created_by',
     ];
 
+    // Auto generate slug if empty
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($course) {
+            if (empty($course->slug)) {
+                $course->slug = Str::slug($course->name);
+            }
+        });
+    }
+
+    /**
+     * Relasi: Course memiliki banyak Module
+     */
     public function modules()
     {
         return $this->hasMany(Module::class);
+    }
+
+    /**
+     * Relasi: Course dibuat oleh Admin (optional)
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 }
