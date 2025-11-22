de51778a703ebef035420b4032f62e3ee30bbef3
Alya Hamidah <lyahamidah14@gmail.com>
2025-11-22 22:08:28 +0700

    Update IsMentee.php

--- ModuLearn/app/Http/Middleware/IsMentee.php
+++ ModuLearn/app/Http/Middleware/IsMentee.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-    public function handle(Request $request, Closure $next)
-    {
+class IsMentee {
+    public function handle($request, Closure $next) {
         if ($request->user() && $request->user()->role === 'mentee') {
             return $next($request);
         }
